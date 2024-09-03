e289720a7653bec24212721f62d8bd3053ef0940
Daniyar <denaeshev@gmail.com>
2024-09-03 23:22:42 +0600

    fix main button error

--- src/telegram/index.tsx
+++ src/telegram/index.tsx
@@ -26,6 +26,10 @@ export const TelegramProvider = ({
     useEffect(() => {
         if (webApp) {
             webApp.expand();
+            webApp.MainButton.setParams({
+                color: "#C285D6",
+                text_color: "#ffffff",
+            });
         }
     }, [webApp]);
 
